c23c8aaa243a707c5033a8250ad09c478dbc3d91
evenX86 <xuyifei@shadagames.com>
2014-07-25 14:05:00 +0800

    [mod] 将count与length的乘机改为256

--- src/main/route/com/alibaba/cobar/route/util/PartitionUtil.java
+++ src/main/route/com/alibaba/cobar/route/util/PartitionUtil.java
@@ -25,7 +25,7 @@ import com.alibaba.cobar.util.StringUtil;
 public final class PartitionUtil {
 
     // 分区长度:数据段分布定义，其中取模的数一定要是2^n， 因为这里使用x % 2^n == x & (2^n - 1)等式，来优化性能。
-    private static final int PARTITION_LENGTH = 1024;
+    private static final int PARTITION_LENGTH = 256;
 
     // %转换为&操作的换算数值
     private static final long AND_VALUE = PARTITION_LENGTH - 1;
